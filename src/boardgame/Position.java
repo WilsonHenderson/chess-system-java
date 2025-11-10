34a8f8a5faa04c0508c0afab501b89129a5689cb
Wilson Henderson <wilsonenderson01@gmail.com>
2025-11-09 23:46:49 -0300

    Implementing possible moves of Rook

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -22,6 +22,12 @@ public class Position {
 		this.column = column;
 	}
 	
+	public void setVelues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	
 	@Override
 	public String toString() {
 		return row + ", " + column;
